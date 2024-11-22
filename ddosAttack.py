060ab056da99470f9aefc4ab67be9a4022346245
Cyber Row-X ™ <121046584+CyberRowX@users.noreply.github.com>
2024-11-22 23:37:47 +0600

    Create ddosAttack.py

---
+++ ddosAttack.py
@@ -0,0 +1,18 @@
+import golay
+import passed 
+import TonyStark
+
+requested = ("TonyStark")+pass
+data = ("DDOS.go") + requested
+
+try:
+  requested from data
+  go from python
+  from get python import data
+pass
+    tony() + go
+    file run from TonyStark():
+Tony()
+
+import cython from cythoniz
+TonyStark()

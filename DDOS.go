3564df95c3f5c0f7e81b3810b679f3488704f228
Cyber Row-X ™ <121046584+CyberRowX@users.noreply.github.com>
2024-11-22 23:21:50 +0600

    Create DDOS.go

---
+++ DDOS.go
@@ -0,0 +1,32 @@
+import(
+  tony from TonyStark-cython.311.os
+  pyload import cython
+  RedZone bypass
+  System Helping import -site
+  )
+
+var (
+	safe            bool     = false
+	headersReferers []string = []string{
+		"nahid",
+		"nahid.py","encrypted",
+		"TonyStark-cython.311.os","pass -data",
+		"http://www.google.ru/?hl=ru&q=",
+		"net bug biggie-TonyStark + print(data)",
+  }
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"sync/atomic"
+	"syscall"
+)
+const __version__  = "1.0.1"
